ccaca532553b137d85b5fed80a97213a055be29a
Caitlin Loeffler <cloeffle@login4.(none)>
2020-02-24 11:42:07 -0800

    Start. Organize code and get draft of code into github for reference.

---
+++ code/get_consensus_stats.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+
+import re
+
+
+with open("four_triple_nine.raw.fa") as f:
+	contig_lengths = []
+	num_contigs = 0
+	min_length = 0
+	max_length = 0
+	avg_length = 0
+	sum_contig_lengths = 0
+	for line in f:
+		if re.findall(r">", line):
+			num_contigs +=1
+			contig_lengths.append(re.findall(r"\d+", line)[1])
+	print(num_contigs, contig_lengths)
+	if len(contig_lengths) >= 1:
+		contig_lengths.sort()
+		min_length = contig_lengths[0]
+		max_length = contig_lengths[-1]
+		for lengths in contig_lengths:
+			sum_contig_lengths += int(lengths)
+		avg_length = sum_contig_lengths / num_contigs
+	print(avg_length, min_length, max_length)
