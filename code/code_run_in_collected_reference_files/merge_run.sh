dc3c02507a94a772d93287af997fc2034d67ce33
cloeffler745 <cloeffle@login2.(none)>
2020-03-12 14:19:46 -0700

    See previous note, actually has the updated merge here.

--- code/code_run_in_collected_reference_files/merge_run.sh
+++ code/code_run_in_collected_reference_files/merge_run.sh
@@ -15,18 +15,40 @@ samtools view aln.sam | grep -P "\t4\t\*" | awk -F '\t' '{print $1}' > get_these
 
 
 # See if the contigs that did not match are the same as each other
-xargs samtools faidx all_reads.fa.gz < get_these.txt > hold.fa # get headers of seq that were not aligned
+# xargs samtools faidx all_reads.fa.gz < get_these.txt > hold.fa # get headers of seq that were not aligned
 
-../../Merge_Databases/code/code_run_in_collected_reference_files/change_headers_for_hold.sh
+number=0
+
+:> hold1.fa
+
+xargs samtools faidx all_reads.fa < get_these.txt | while read -r line; do 
+        if $(echo $line | grep -q ">")
+        then
+                printf '%s%i\n' ">Read" "$(( ++number ))" >> hold1.fa
+        else
+                echo $line >> hold1.fa
+        fi
+done 
+
+
+awk '/^>/ { if (name) {printf("%s len=%d\n%s", name, len, seq)} name=$0; seq=""; len = 0; next}
+    NF > 0 {seq = seq $0 "\n"; len += length()}
+    END { if (name) {printf("%s len=%d\n%s", name, len, seq)} }' hold1.fa > hold.fa
+
+rm hold1.fa
+
 
 /u/home/c/cloeffle/project/anaconda2/bin/ete3_apps/bin/muscle -in hold.fa -clwstrictout aligned.clw  # run multiple sequence alignment on the unaligned seq
 /u/home/c/cloeffle/project/anaconda2/bin/ete3_apps/bin/clustalo -i aligned.clw --percent-id --distmat-out=pim.txt --full --force # get the percent identity matrix
 
 ../find_uniq_of_unaln.py # find unique reads that were not alinged , puts terms in "unaln_uniq.txt"
 
+xargs samtools faidx all_reads.fa < unaln_uniq.txt > hold.fa # change unalinged reads so that it only holds unique references (remove duplicates)
+
+
 # combine the raw files and the extra bits into one file
 
-../../Merge_Databases/code/code_run_in_collected_reference_files/make_master_reference.sh
+cat assembly.raw.fa hold.fa > master_ref.fa
 
 gzip master_ref.fa
 
