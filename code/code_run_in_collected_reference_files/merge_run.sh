435582389a1559552eb39e11eb6dafad5db23dd6
cloeffler745 <cloeffle@login2.(none)>
2020-03-12 14:14:44 -0700

    Combined run_my_scripts, next_steps, change_headers_for_hold, and make_master_reference into one file merge_run. Also edited next_steps part so that we used the headers generated by find_uniq_of_unaln.py was actually used to eliminate duplicates of unaln reads before adding the rerferences to the master set of contigs.

---
+++ code/code_run_in_collected_reference_files/merge_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+. /u/local/Modules/default/init/modules.sh
+
+#../../Merge_Databases/code/collect_reads.sh
+../../Merge_Databases/code/run_assembly_tool.sh
+module load bwa
+bwa index assembly.raw.fa
+module load samtools
+bwa mem assembly.raw.fa all_reads.fa.gz  > aln.sam
+
+samtools view aln.sam | awk '{print $1,$2,$3}' > important_data_from_aln_file.txt
+
+samtools view aln.sam | grep -P "\t4\t\*" | awk -F '\t' '{print $1}' > get_these.txt # Will get the first part of the first line of the files that were not aligned.
+
+
+# See if the contigs that did not match are the same as each other
+xargs samtools faidx all_reads.fa.gz < get_these.txt > hold.fa # get headers of seq that were not aligned
+
+../../Merge_Databases/code/code_run_in_collected_reference_files/change_headers_for_hold.sh
+
+/u/home/c/cloeffle/project/anaconda2/bin/ete3_apps/bin/muscle -in hold.fa -clwstrictout aligned.clw  # run multiple sequence alignment on the unaligned seq
+/u/home/c/cloeffle/project/anaconda2/bin/ete3_apps/bin/clustalo -i aligned.clw --percent-id --distmat-out=pim.txt --full --force # get the percent identity matrix
+
+../find_uniq_of_unaln.py # find unique reads that were not alinged , puts terms in "unaln_uniq.txt"
+
+# combine the raw files and the extra bits into one file
+
+../../Merge_Databases/code/code_run_in_collected_reference_files/make_master_reference.sh
+
+gzip master_ref.fa
+
+echo "Done!"
