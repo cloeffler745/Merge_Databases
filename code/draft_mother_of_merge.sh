8a8e7498efcadb88c987381a01953b0a46cc923b
cloeffler745 <cloeffle@login1.(none)>
2020-03-11 17:15:44 -0700

    fixing content.txt and started draft_mother_of_merge.sh with pseudo code

---
+++ code/draft_mother_of_merge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Locate all files and gather file paths
+
+
+# Cat references together, with new headers
+
+
+# align to find unmapped pieces 
+
+
+# get percent identity matrix
+
+
+# find which files/seq are unique  among unmapped reads
+
+
+# Compile assemblies and unique unaligned contigs into 1 file
+
+
+# Create new SQL database with master DB stats
+
+
+
